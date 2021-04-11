f672a53d23fd80ee6be740e884906e8605ea543b
ourongxing <orongxing@gmail.com>
2021-04-11 17:00:59 +0800

    add more return value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 #  create by ourongxing
-#  detail url: https://github.com/ourongxing/login4cqupt
-
+#  detail url: https://github.com/ourongxing/login4cqupt 
 import requests
 import argparse
 import socket
@@ -18,14 +17,21 @@ def get_ip():
 def login(ip, args):
     args.ip = ip
     args.device = 0 if args.device == 'pc' else 1
-    for i in range(5):
-        res = requests.get('http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C{device}%2C{account}%40{operator}&user_password={password}&wlan_user_ip={ip}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name='.format_map(vars(args)))
-        if '"msg":""' in res.text:
-            print('您已经登录')
-            return
-        elif r'\u8ba4\u8bc1\u6210\u529f' in res.text:
-            print('可以上网了')
-            return
+    res = requests.get('http://192.168.200.2:801/eportal/?c=Portal&a=login&callback=dr1003&login_method=1&user_account=%2C{device}%2C{account}%40{operator}&user_password={password}&wlan_user_ip={ip}&wlan_user_ipv6=&wlan_user_mac=000000000000&wlan_ac_ip=&wlan_ac_name='.format_map(vars(args)))
+    if '"msg":""' in res.text:
+        print('当前设备已登录')
+        return
+    elif r'\u8ba4\u8bc1\u6210\u529f' in res.text:
+        print('登录成功')
+        return
+    elif 'bGRhcCBhdXRoIGVycm9y' in res.text:
+        print("密码错误")
+        return
+    elif 'aW51c2UsIGxvZ2luIGFnYWluL' in res.text:
+        login(get_ip(), get_args())
+    else:
+        print("您可能欠费停机")
+        return 
 
 def get_args():
     parser = argparse.ArgumentParser(
